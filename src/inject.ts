1b770c994051147e5f6c7a6404958da18b80cb2f
Phil Schaffarzyk <ps@gridventures.de>
2021-11-17 16:40:04 +0100

    feat: export init function

---
+++ src/inject.ts
@@ -0,0 +1,3 @@
+import CookieConsent from './main';
+
+CookieConsent();
--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,9 @@ import { render } from 'preact';
 import { App } from './app';
 import './index.css';
 
-const root = document.getElementById('cc');
-if (root) render(<App />, root);
+const renderCookieConsent = () => {
+  const root = document.getElementById('cc');
+  if (root) render(<App />, root);
+};
+
+export default renderCookieConsent;
