d790f6e76d1f802dd48ffda3d1072e164c5ea48a
Phil <phil.schaffarzyk@gridventures.de>
2023-11-10 12:59:33 +0100

    fix: make package usable inside nextjs projects

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,5 +1,10 @@
 const c = console;
-const d = () => window.CC_DEBUG;
+const d = () => {
+  if (typeof window !== 'undefined') {
+    return window.CC_DEBUG;
+  }
+  return false;
+};
 const f = (...arg: any[]) => {}; // eslint-disable-line @typescript-eslint/no-unused-vars
 export default {
   log: d() ? c.log : f,
