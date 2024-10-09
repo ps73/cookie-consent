8b0d6d76bb9e3b9983c895c6af9cdd0924eafbea
Phil <phil.schaffarzyk@gmail.com>
2024-10-09 15:51:36 +0200

    fix: prettier error

--- src/hooks/useClasses.ts
+++ src/hooks/useClasses.ts
@@ -8,14 +8,14 @@ export function getPositionClass(p: PanelPosition) {
   return p === 'bl' // bottom left
     ? ' b l'
     : p === 'bc' // bottom center
-    ? ' b c'
-    : p === 'cc' // center center
-    ? ' cc'
-    : p === 'tl' // top left
-    ? ' t l'
-    : p === 'tr' // top right
-    ? ' t r'
-    : p === 'tc' // top center
-    ? ' t c'
-    : ' b r'; // fallback: bottom right
+      ? ' b c'
+      : p === 'cc' // center center
+        ? ' cc'
+        : p === 'tl' // top left
+          ? ' t l'
+          : p === 'tr' // top right
+            ? ' t r'
+            : p === 'tc' // top center
+              ? ' t c'
+              : ' b r'; // fallback: bottom right
 }
